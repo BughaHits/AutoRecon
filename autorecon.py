2900c7074d6fe0861a56ddd3427b1d4c66eb3215
Shyam ! <sbas0028@student.monash.edu>
2023-08-06 17:21:51 +1000

    nmap and subnet recon

---
+++ autorecon.py
@@ -0,0 +1,16 @@
+import os
+from pynput.keyboard import Key, Controller
+
+keyboard = Controller()
+
+
+ip = input("Please enter a target: ")
+
+os.system(f"nmap -A -p {ip} -v >> nmap.txt")
+
+os. system(f"smbclient -N -L {ip} >> smbclient.txt")
+
+os. system(f"smbmap -H {ip} >> smbmap.txt")
+
+keyboard.press(Key.enter)
+keyboard.release(Key.enter)
